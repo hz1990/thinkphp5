a783b6aae70c3af498bfc4638ac2791f6e553f53
hz1990 <scbzhezhi@163.com>
2016-12-12 10:19:17 +0800

    first uplaod
    upload and download

---
+++ application/index/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use think\Db;
+use think\View;
+
+class Index extends Controller
+{
+    public function index()
+    {
+       /* $data=Db::name('admin')->select();
+        echo '<pre>';
+        print_r($data);*/
+        //return '<p>this is tp5 index</p>';
+        $view = new View();
+        return $view->fetch('index');
+    }
+}
